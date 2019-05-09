e291e806c7b00e1980db739432a274fb763a8ef0
gjakers <36972988+gjakers@users.noreply.github.com>
2019-05-09 12:13:42 -0400

    Create reddit_test.js

---
+++ reddit_test.js
@@ -0,0 +1,30 @@
+const request   = require('request');
+const xml2js    = require('xml2js');
+
+var parser = new xml2js.Parser();
+
+const options = {
+  url: "https://www.reddit.com/r/bestguyever.rss",
+  headers: {
+    'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) " +
+                  "AppleWebKit/537.36 (KHTML, like Gecko) " +
+                  "Chrome/62.0.3202.94 " +
+                  "Safari/537.36"
+  }
+};
+
+request(options, function(err, response, body) {
+	parser.parseString(body, function(err, data) {
+		data['feed']['entry'].forEach( function(entry) {
+			r.getSubreddit('pizza_friends').submitSelfpost(
+				{title: 'test',
+				 text: entry['content']
+				}
+			);
+
+		
+		
+		
+		});
+	});
+});
